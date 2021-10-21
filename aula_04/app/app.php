243d2568479d9b1997be40f10ec483ae851755d1
AmiloOscar <aoscar@inss.gov.mz>
2021-10-21 21:30:59 +0200

    MELHORIAS NA API E NA APP

--- aula_04/app/app.php
+++ aula_04/app/app.php
@@ -4,10 +4,19 @@ define('API_BASE', 'http://localhost:8080/curso_api/aula_04/api/index.php?option
 
 echo '<p>APLICAÇÃO</p>';
 
-$resultado = api_request('status');
+for($i=0; $i<10; $i++)
+{
+    $resultado = api_request('random');
 
-echo '<pre>';
-print_r($resultado);
+    //verify if response is ok (success)
+    if($resultado['status'] == 'ERROR'){
+        die('Aconteceu um erro na minha chamada a API.');
+    }
+
+    echo "O valor randómico: " . $resultado['data'] . "<br>";
+}
+
+echo 'TERMINADO';
 
 function api_request($option)
 {
